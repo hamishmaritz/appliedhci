9f23415b4cde9d1609a6f659425d32e7fbb08f98
hamishmaritz <hamishcmaritz@gmail.com>
2019-05-28 20:42:39 +1200

    Basic Styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
+import Main from "./Main"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-      </header>
+      
+      <Main />
     </div>
   );
 }
